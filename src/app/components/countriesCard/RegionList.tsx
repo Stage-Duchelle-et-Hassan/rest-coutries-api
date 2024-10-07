144ff35326ca78d295249b76a4f0667344f4263c
duchelle5093 <duchellemboutchouang@gmail.com>
2024-10-07 18:16:17 +0100

    Add an amimation while loading data

--- src/app/components/countriesCard/RegionList.tsx
+++ src/app/components/countriesCard/RegionList.tsx
@@ -1,13 +1,18 @@
 export const RegionList  = (props) =>{
 
     const continents = ['All', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+    
+    const removeRegionList = (continent : string )=>{
+        props.setSelectedRegion(continent === 'All' ? undefined : continent.toLowerCase())
+        props.setShowRegionList(false)
+    }
         
     return(
            <div className=" flex flex-col items-start mt-2 bg-primary-foreground  shadow-md rounded-sm absolute z-50 w-48 ">
                 {continents.map((continent) => (
                     <button className="hover:bg-slate-600 hover:text-white w-full text-start pl-4 py-2 text-primary"
                         key={continent}
-                        onClick={() => props.setSelectedRegion(continent === 'All' ? undefined : continent.toLowerCase())}
+                        onClick={()=> removeRegionList(continent)}
                     >
                         {continent}
                     </button>
--- src/app/components/countriesCard/fetchCountriesData.tsx
+++ src/app/components/countriesCard/fetchCountriesData.tsx
@@ -30,22 +30,31 @@ export interface Country {
     const [showRegionList , setShowRegionList] = useState(false)
     const [selectedRegion, setSelectedRegion] = useState<string | undefined>(undefined);
 
-    const { data } = useQuery<Country[]>({
+    const { data ,  isLoading } = useQuery<Country[]>({
       queryKey: ['countries', selectedRegion],
       queryFn: () => fetchCountries(selectedRegion),
     });
     
-      
-   
+
+      if(isLoading){
+        return(
+          <div className="flex flex-row items-center justify-center gap-2 w-full h-96">
+            <div className="w-6 h-6 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+            <div className="w-6 h-6 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
+            <div className="w-6 h-6 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+          </div>
+        )
+      }
+        
       return (
         <>
         <div className=' flex flex-col gap-48 items-end'>
-          <div className=' fixed top-28 right-48 z-50'>
+          <div className=' absolute top-28 right-48 z-50'>
             <div className="  flex justify-between items-center bg-primary-foreground shadow-md w-48 px-4 h-9 cursor-pointer  rounded-sm" onClick={()=> setShowRegionList(!showRegionList)}>
                 <p className=' text-primary'>Filter by region</p>
                 <ChevronDown className=' text-primary'/>
             </div>
-            {showRegionList && <RegionList setSelectedRegion = {setSelectedRegion}/>}
+            {showRegionList && <RegionList setSelectedRegion = {setSelectedRegion} setShowRegionList={setShowRegionList}/>}
           </div>
           <div className=' flex text-black' onClick={()=> setShowRegionList(false)}>
             <div className=' flex gap-16 flex-wrap items-center justify-center'>
