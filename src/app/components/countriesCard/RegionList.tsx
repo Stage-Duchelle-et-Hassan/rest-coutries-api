4f86fa021bbc2b3eafd2376fa2a5e8e0df6aa44b
Hassan <hassanmahamatdogo@gmail.com>
2024-10-15 08:26:39 +0100

    Mettre en place le responsive

--- src/app/components/countriesCard/RegionList.tsx
+++ src/app/components/countriesCard/RegionList.tsx
@@ -15,7 +15,7 @@ export const RegionList  = (props  : RegionListType) =>{
     }
         
     return(
-           <div className=" flex flex-col items-start mt-2 bg-primary-foreground  shadow-md rounded-sm absolute z-50 w-48 ">
+           <div className=" flex flex-col items-end bg-primary-foreground  shadow-md rounded-sm absolute z-50 w-48 ">
                 {continents.map((continent) => (
                     <button className="hover:bg-slate-600 hover:text-white w-full text-start pl-4 py-2 text-primary"
                         key={continent}
